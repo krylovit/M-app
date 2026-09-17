17ca6e4a8b50ee06e549c040b54dd4ed04a8ed5c
krylovit <krylovit@gmail.com>
2026-09-18 01:09:54 +0300

    Window redesigned per reference: two reel portholes + center meter, height-relative proportions

--- app.js
+++ app.js
@@ -1856,8 +1856,13 @@ document.querySelectorAll('.item').forEach(function(el) {
                     <div class="cassette-sub">МК 60-5 · ГОСТ 137-1973/128 · Цена 4 руб. · 03.1990</div>
                     <div class="cassette-stripes"></div>
                     <div class="cassette-window">
-                        <div class="reel-wrap"><div class="tape"></div><div class="reel" id="reelL"></div></div>
-                        <div class="reel-wrap"><div class="tape"></div><div class="reel" id="reelR"></div></div>
+                        <div class="porthole">
+                            <div class="reel-wrap"><div class="tape"></div><div class="reel" id="reelL"></div></div>
+                        </div>
+                        <div class="meter"></div>
+                        <div class="porthole">
+                            <div class="reel-wrap"><div class="tape"></div><div class="reel" id="reelR"></div></div>
+                        </div>
                     </div>
                     <div class="cassette-track" id="radioTrack"></div>
                 </div>
